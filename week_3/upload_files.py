d4efb290c169929984c0a303e4e2374a1a61bd43
Abdullah Butt <abdullah0306533@gmail.com>
2026-05-13 14:41:48 +0500

    week-04: FastAPI error handling with HTTPException and file type validation

--- week_3/upload_files.py
+++ week_3/upload_files.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI,File,UploadFile
+from fastapi import FastAPI, UploadFile, HTTPException
 
 app = FastAPI()
 @app.post("/upload_files")
 async def upload_files(file: UploadFile):
+    if file.content_type not in ["image/jpeg","image/png","image/jpg"] :
+        raise HTTPException(status_code=400,detail="Invalid file type")
     return {"filename": file.filename,"file size":file.size,"content_type":file.content_type}
